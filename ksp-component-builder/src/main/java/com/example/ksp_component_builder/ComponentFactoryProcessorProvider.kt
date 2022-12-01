4181bb3f49ad842e245b2de835c6169acf4154c8
umpteenthdev <>
2022-12-01 08:25:54 +0300

    add test parameters and log environment

--- ksp-component-builder/src/main/java/com/example/ksp_component_builder/ComponentFactoryProcessorProvider.kt
+++ ksp-component-builder/src/main/java/com/example/ksp_component_builder/ComponentFactoryProcessorProvider.kt
@@ -7,6 +7,15 @@ import com.google.devtools.ksp.processing.SymbolProcessorProvider
 internal class ComponentFactoryProcessorProvider : SymbolProcessorProvider {
 
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        environment.logger.warn("""
+            ComponentFactoryProcessorProvider.create
+             |kotlinVersion: ${environment.kotlinVersion}
+             |apiVersion: ${environment.apiVersion}
+             |compilerVersion: ${environment.compilerVersion}
+             |platforms: ${environment.platforms}
+             |options: ${environment.options}
+        """.trimIndent())
+
         return ComponentFactoryProcessor(
             logger = environment.logger,
             fileGenerator = ComponentFactoryFileGenerator(
