fc4049fbe2cf7c6ab8b0fb8adff4f8c527288e1a
umpteenthdev <andrei.erdman.27@gmail.com>
2024-03-07 06:45:10 +0300

    bump dependencies' versions

--- contacts/src/main/java/com/example/contacts/AllDefaultParametersModule.kt
+++ contacts/src/main/java/com/example/contacts/AllDefaultParametersModule.kt
@@ -1,10 +1,17 @@
 package com.example.contacts
 
 import dagger.Module
+import dagger.Provides
 import java.util.UUID
 
 @Module
 internal class AllDefaultParametersModule(
     private val key: String = UUID.randomUUID().toString(),
     private val timestamp: Long = System.currentTimeMillis(),
-)
+) {
+
+    @Provides
+    fun provideUniqueId(): String {
+        return key
+    }
+}
--- contacts/src/main/java/com/example/contacts/RequiredParameterModule.kt
+++ contacts/src/main/java/com/example/contacts/RequiredParameterModule.kt
@@ -1,8 +1,16 @@
 package com.example.contacts
 
 import dagger.Module
+import dagger.Provides
+import java.io.File
 
 @Module
 internal class RequiredParameterModule(
     private val activity: ContactsActivity,
-)
+) {
+
+    @Provides
+    fun provideCacheDir(): File {
+        return activity.cacheDir
+    }
+}
